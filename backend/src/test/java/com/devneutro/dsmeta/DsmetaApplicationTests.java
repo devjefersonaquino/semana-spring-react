1154e46a799d2c2176a4acfc4f7635093435d4f6
Jeferson Aquino <jefersonaquino@pop-os.localdomain>
2022-07-12 13:17:15 -0300

    Estrutura do Projeto.

---
+++ backend/src/test/java/com/devneutro/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devneutro.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
